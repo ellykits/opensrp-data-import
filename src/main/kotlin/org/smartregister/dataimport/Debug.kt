7e193b43a2cdd09c0064f8407e4a3ecf72351aa7
Elly Kitoto <junkmailstoelly@gmail.com>
2021-05-26 12:09:51 +0300

    Migrate to logger to log4j2

--- src/main/kotlin/org/smartregister/dataimport/Debug.kt
+++ src/main/kotlin/org/smartregister/dataimport/Debug.kt
@@ -15,7 +15,7 @@ import org.smartregister.dataimport.shared.*
 
 fun main() {
   val configs = JsonObject().apply {
-    put(IMPORT_OPTION, "practitioners")
+    put(IMPORT_OPTION, "locations")
 //    put(SOURCE_FILE, "assets/locations.csv")
 //    put(USERS_FILE, "assets/users.csv")
 //    put(SKIP_USER_GROUP, true)
