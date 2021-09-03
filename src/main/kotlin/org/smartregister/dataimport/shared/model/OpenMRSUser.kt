99059fc6903cc83cf236276c5c6db509844f85f0
Elly Kitoto <junkmailstoelly@gmail.com>
2021-09-03 20:49:02 +0300

    Annotate OpenMRSUser with @Serializable
    Signed-off-by: Elly Kitoto <junkmailstoelly@gmail.com>

--- src/main/kotlin/org/smartregister/dataimport/shared/model/OpenMRSUser.kt
+++ src/main/kotlin/org/smartregister/dataimport/shared/model/OpenMRSUser.kt
@@ -1,8 +1,10 @@
 package org.smartregister.dataimport.shared.model
 
 import com.opencsv.bean.CsvBindByPosition
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 
+@Serializable
 class OpenMRSUser {
   @CsvBindByPosition(position = 0)
   var username: String = ""
